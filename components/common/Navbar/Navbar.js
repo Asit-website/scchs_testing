dd52148e8b20859a1c1230b35a585fa3ad6868f6
asitmandal <asitmandal492@gmail.com>
2025-06-17 15:31:46 +0530

    this

--- components/common/Navbar/Navbar.js
+++ components/common/Navbar/Navbar.js
@@ -773,8 +773,8 @@ export default function Navbar(props) {
                           <ol>
                            <a href="/research4"> <li>SCCHS Heritage Journal</li></a>
                            <a href="/heritage"> <li>SCCHS Newsletters</li></a>
-                             <a href="/heritage"> <li>Genealogy Newsletter</li></a>
-                            <a href="/genealogy-newsletter"> <li>SCCHS President Messages</li></a>
+                             <a href="/genealogy-newsletter"> <li>Genealogy Newsletter</li></a>
+                            <a href="/message-1"> <li>SCCHS President Messages</li></a>
                           </ol>
                         </span>
                       }
@@ -1148,15 +1148,17 @@ export default function Navbar(props) {
 
                   {subOpen && (
                     <div className="member-sub-dropdown">
-                      <div className="member-sub-item">Membership Newsletters</div>
-                      <div className="member-sub-item">SCCHS Genealogy Newsletters</div>
+                       <a href={"/research4"}><div className="member-sub-item">SCCHS Heritage Journal</div></a>
+                     <a href={"/heritage"}><div className="member-sub-item">Scchs Newsletters</div></a>
+                     <a href="/genealogy-newsletter"><div className="member-sub-item">SCCHS Genealogy Newsletters</div></a>
+                      <a href="/message-1"><div className="member-sub-item">SCCHS President Message</div></a>
                     </div>
                   )}
 
                  <a style={{textDecoration:"none",color:"white"}} href="/member/myprofile"><div className="member-dropdown-item">My Profile</div></a> 
-                  <div className="member-dropdown-item">Research</div>
-                  <div className="member-dropdown-item">Cementry Records</div>
-                  {/* <div className="member-dropdown-item">Membership List</div> */}
+                 <a style={{textDecoration:"none",color:"white"}} href="/research"><div className="member-dropdown-item">Research</div></a>
+                 <a style={{textDecoration:"none",color:"white"}} href="/cementryrecord"><div className="member-dropdown-item">Cementry Records</div></a>
+                 <a style={{textDecoration:"none",color:"white"}} href="/member/memberlist"><div className="member-dropdown-item">Membership List</div></a>
                 </div>
               )}
             </div>}
