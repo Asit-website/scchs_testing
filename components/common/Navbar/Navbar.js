a50af9e0cf4560a0ea377ad387df9ffe033b6351
asitmandal <asitmandal492@gmail.com>
2025-07-03 17:34:27 +0530

    this

--- components/common/Navbar/Navbar.js
+++ components/common/Navbar/Navbar.js
@@ -670,7 +670,7 @@ export default function Navbar(props) {
     }, []);
     const toggleDropdown112 = () => setIsOpen1(!isOpen1);
 
-    
+
 
     return (
       <>
@@ -1003,7 +1003,7 @@ export default function Navbar(props) {
                 </li>
 
               }
-           {instaUser && <li className="test_sign"><a>Hi <span style={{fontWeight:"bold"}}>{instaUser?.first_name}</span></a></li>}   
+              {instaUser && <li className="test_sign"><a>Hi <span style={{ fontWeight: "bold" }}>{instaUser?.first_name}</span></a></li>}
             </ul>
           </div>
         </div>
