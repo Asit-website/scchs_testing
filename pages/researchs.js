c5664c9a2dc34e8f14256ea6cb71eca7baa4c547
Rajatbora869 <rajatbora869@gmail.com>
2025-07-05 17:29:18 +0530

    this

--- pages/researchs.js
+++ pages/researchs.js
@@ -114,7 +114,7 @@ export default function researchs(pageProp) {
                                         <li><a href={membershipStatus === "active" ? "/Br-permit" : "/join/memberplan"}>Burial or Removal Permits</a></li>
                                         <li><a href={membershipStatus === "active" ? "/Burials-By-Church" : "/join/memberplan"}>Burials by Church</a></li>
                                         {/* https://admin.scchs.co.in/backend/admin/media/Business%20%26%20Industry%20Files%20Index/Business%20%26%20Industry%20Files%20Index.pdf */}
-                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/Business%20%26%20Industry%20Files%20Index/Business%20%26%20Industry%20Files%20Index.pdf" : "/join/memberplan"}>Business &amp; Industry Files Index</a></li>
+                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/Business%20%26%20Industry%20Files%20Index/Business%20%26%20Industry%20Files%20Index.pdf" : "/join/memberplan"}target="_blank">Business &amp; Industry Files Index</a></li>
                                         <li>
                                             <a href={membershipStatus === "active" ? "/cementryrecord" : "/join/memberplan"}>Cemetery Records</a>
                                             <p className="cementry-text">There are currently almost 50,000 individual records available from over 350 cemeteries in Saint Charles</p>
@@ -124,22 +124,35 @@ export default function researchs(pageProp) {
 
                                         <li><a href={membershipStatus === "active" ? "	/census" : "/join/memberplan"}>Census, St. Charles County, 1876 Family Files</a></li>
                                         {/*  */}
-                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/Family%20Files/Family%20Files.pdf" : "/join/memberplan"}>Family Files</a></li>
+                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/Family%20Files/Family%20Files.pdf" : "/join/memberplan"}target="_blank">Family Files</a></li>
                                         <li><a href={membershipStatus === "active" ? "/guardian-book-search" : "/join/memberplan"}>Guardian Books</a></li>
                                         <li><a href={membershipStatus === "active" ? "/land-and-property" : "/join/memberplan"}>Land and Property Record by Last Name</a></li>
                                         <li><a href={membershipStatus === "active" ? "/land-and-property 1" : "/join/memberplan"}>Land and Property Records by Year</a></li>
                                         <li><a href={membershipStatus === "active" ? "	/marriages-by-church" : "/join/memberplan"}>Marriages by Church</a></li>
                                         <li><a href={membershipStatus === "active" ? "/marriage1" : "/join/memberplan"}>Marriage Index 1836 - 1858</a></li>
-                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/New_data/St.%20Charles%20County%20Historical%20Society%20-%20McElhiney%20Olson%20Index.pdf " : "/join/memberplan"}>McElhiney Olson Historical Newspaper Articles Index</a></li>
+                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/New_data/St.%20Charles%20County%20Historical%20Society%20-%20McElhiney%20Olson%20Index.pdf " : "/join/memberplan"}target="blank">McElhiney Olson Historical Newspaper Articles Index</a></li>
                                         <li><a href={membershipStatus === "active" ? "/naturalization-records" : "/member/memberlogin"}>Naturalization Records</a></li>
                                         <li><a href={membershipStatus === "active" ? "	/obituaries" : "/member/memberlogin"}>Obituaries</a></li>
-                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/New_data/St.%20Charles%20County%20Historical%20Society%20-%20Demokrat%20obits%201852-1894.pdf " : "/join/memberplan"}>Obituaries from the Demokrat w/English translations (1852-1894)</a></li>
+                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/New_data/St.%20Charles%20County%20Historical%20Society%20-%20Demokrat%20obits%201852-1894.pdf " : "/join/memberplan"} target="_blank">Obituaries from the Demokrat w/English translations (1852-1894)</a></li>
                                         <li><a href={membershipStatus === "active" ? "/Probate Records" : "/member/memberlogin"}>Probate Records</a></li>
                                         <li><a href={membershipStatus === "active" ? "/Church-Baptisms	" : "/member/memberlogin"}>Selected Church Baptisms</a></li>
-                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/hisrical_record/St.%20Charles%20County%20Historical%20Society%20-%20Sibley%20Diary%201844-1855.pdf " : "/join/memberplan"}>Sibley Diary 1844 - 1855</a></li>
+                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/hisrical_record/St.%20Charles%20County%20Historical%20Society%20-%20Sibley%20Diary%201844-1855.pdf " : "/join/memberplan"}target="_blank">Sibley Diary 1844 - 1855</a></li>
                                         {/* https://admin.scchs.co.in/backend/admin/media/Stillbirths.pdf */}
                                         <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/Stillbirths.pdf" : "/join/memberplan"}>Stillbirths</a></li>
-                                        <li><a href={membershipStatus === "active" ? "https://admin.scchs.co.in/backend/admin/media/St.%20Charles%20County%20Historical%20Society%20-%20Subject_Topic%20Files.pdf " : "/join/memberplan"}>Subject/Topic Files</a></li>
+                                        <li>
+                                            <a
+                                                href={
+                                                    membershipStatus === "active"
+                                                        ? "https://admin.scchs.co.in/backend/admin/media/St.%20Charles%20County%20Historical%20Society%20-%20Subject_Topic%20Files.pdf"
+                                                        : "/join/memberplan"
+                                                }
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                Subject/Topic Files
+                                            </a>
+                                        </li>
+
                                         <li><a href={membershipStatus === "active" ? "/world-war" : "/join/memberplan"}>World War II Marriages</a></li>
                                     </ul>
                                 </div>
@@ -187,14 +200,14 @@ export default function researchs(pageProp) {
                                     <p>
                                         2. The&nbsp;St. Charles County Circuit Court online index&nbsp;is the result of a-partnership between the Local
                                         Records Division of the Missouri State Archives, the St. Charles County' circuit clerk and the
-                                        SCCHS.&nbsp; See <a href="https://admin.scchs.co.in/backend/admin/media/scchs/Historical_Research_with_St._Charles_County_Circuit_Court_Records.pdf" download="download">Historical Research with St Charles County Circuit Court Records</a>&nbsp;for more
+                                        SCCHS.&nbsp; See <a href="https://admin.scchs.co.in/backend/admin/media/scchs/Historical_Research_with_St._Charles_County_Circuit_Court_Records.pdf" target="_blank">Historical Research with St Charles County Circuit Court Records</a>&nbsp;for more
                                         information.&nbsp;&nbsp;The index:
                                     </p>
 
                                     <ul>
                                         <li>1. includes sessions held&nbsp;from 1805 through 1893 (includes the period St. Charles served as Missouri's state capital),</li>
                                         <li>2. is&nbsp;to&nbsp;original records&nbsp;that are stored at the SCCHS Archives, and</li>
-                                        <li>3. may&nbsp;be&nbsp;searched&nbsp;free at&nbsp;<a href="https://s1.sos.mo.gov/records/archives/archivesdb/JudicialRecords">MO Judicial Archives</a>&nbsp;as part of the Missouri Judicial Records Collection (select Saint&nbsp;Charles in the County box).&nbsp;</li>
+                                        <li>3. may&nbsp;be&nbsp;searched&nbsp;free at&nbsp;<a href="https://s1.sos.mo.gov/records/archives/archivesdb/JudicialRecords" target="_blank">MO Judicial Archives</a>&nbsp;as part of the Missouri Judicial Records Collection (select Saint&nbsp;Charles in the County box).&nbsp;</li>
                                         <li>4. The original documents are at the St. Charles County Historical Society Archives.</li>
                                     </ul>
 
